3819c30ab58b07b507ad314d6b4f875b4b211dd3
SewerSnake <ericgroseclos@hotmail.com>
2017-11-08 15:01:11 +0100

    DBHelper created

--- app/src/main/java/com/example/sqliteandroid/DBHelper.java
+++ app/src/main/java/com/example/sqliteandroid/DBHelper.java
@@ -4,22 +4,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by ericgroseclos on 2017-11-08.
- */
-
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final int DB_VERSION = 1;
 
     public DBHelper(Context context) {
         super(context, "MyDataBase", null, DB_VERSION);
-
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        String sql = "CREATE TABLE Highscores ( id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT NOT NULL," +
+                "points INTEGER);";
+
+        db.execSQL(sql);
     }
 
     @Override
